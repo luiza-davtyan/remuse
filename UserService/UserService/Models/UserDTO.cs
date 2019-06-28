94ad87bc04051f047bc6efec54ac318dc5dafc48
Luiza Davtyan <luizadavtyan@outlook.com>
2019-06-28 22:48:31 +0400

    add user class to models, create music service, create book service

---
+++ UserService/UserService/Models/UserDTO.cs
@@ -0,0 +1,27 @@
+﻿using BookService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Models
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int RoleId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public List<BookDTO> Books { get; set; }
+    }
+}
