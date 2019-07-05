1bb51894723839aabf3a4cd3b404314a058ebb55
Luiza Davtyan <luizadavtyan@outlook.com>
2019-07-05 20:19:47 +0400

    create book service

---
+++ UserService/BookService/Controllers/AuthorController.cs
@@ -0,0 +1,77 @@
+﻿using BookService.Models;
+using BookService.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly AuthorRepository _authorRepository;
+
+        public AuthorController(AuthorRepository authorService)
+        {
+            _authorRepository = authorService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Author>> Get() =>
+            _authorRepository.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetAuthor")]
+        public ActionResult<Author> Get(string id)
+        {
+            var author = _authorRepository.Get(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        [HttpPost]
+        public ActionResult<Author> Create(Author author)
+        {
+            _authorRepository.Create(author);
+
+            return CreatedAtRoute("GetAuthor", new { id = author.Id.ToString() }, author);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Author authorIn)
+        {
+            var author = _authorRepository.Get(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _authorRepository.Update(id, authorIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var author = _authorRepository.Get(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _authorRepository.Remove(author.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ UserService/BookService/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BookService.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [JsonProperty("Title")]
+        [BsonRequired]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Description { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public int Year { get; set; }
+
+        public string Path { get; set; }
+
+        public virtual List<ObjectId> GenreIds { get; set; }
+    }
+}
+
