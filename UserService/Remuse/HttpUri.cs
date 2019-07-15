a658dba70ea011ac9f94c3288ff811eae11f70e6
Serob Hayrapetyan <serob.airapetyan@mail.ru>
2019-07-15 17:42:08 +0400

    bla

--- UserService/Remuse/HttpUri.cs
+++ UserService/Remuse/HttpUri.cs
@@ -2,7 +2,10 @@
 {
     public static class HttpUri
     {
-        public static string BookUri = "";
-        public static string UserUri = "http://192.168.88.72:45455/";
+        public static string IdentityUri = "http://192.168.88.72:45455/";
+        public static string BookUri = "http://192.168.88.72:45457/";
+        public static string UserUri = "http://192.168.88.72:45457/";
+        public static string ProfileApiUri = "";
+        public static string AdminUri = "";
     }
 }
--- UserService/Remuse/SignIn.cs
+++ UserService/Remuse/SignIn.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Widget;
 using MyNamespace;
-using Remuse.Activities;
-using System.Xml.Serialization;
-using System.Threading.Tasks;
-using System.Net.Http;
 using Newtonsoft.Json;
+using Remuse.Activities;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using System.Net.Http.Headers;
+using Remuse.Models;
 
 namespace Remuse
 {
@@ -20,7 +19,7 @@ namespace Remuse
     public class SignIn : Activity
     {
         User userFromBase = new User();
-        TextView correction;
+        TextView correction, check;
         EditText username, password;
         Button signin;
         List<User> users = new List<User>();
@@ -34,7 +33,7 @@ namespace Remuse
             //Creating email's and password's fields
             username = FindViewById<EditText>(Resource.Id.editText1);
             password = FindViewById<EditText>(Resource.Id.textInputEditText1);
-
+            check = FindViewById<TextView>(Resource.Id.textView9);
             signin = FindViewById<Button>(Resource.Id.button1);
             signin.Click += Signin_Click;
         }
@@ -52,37 +51,55 @@ namespace Remuse
             correction = FindViewById<TextView>(Resource.Id.textView8);
             if (username.Text == "")
             {
-                correction.Text = "Please,enter email";
+                correction.Text = "Please,enter username";
             }
             else if (password.Text == "")
             {
                 correction.Text = "Please,enter password";
             }
-
             else
             {
-                //var service = new UserClient(new System.Net.Http.HttpClient());
-                //var result = await service.GetUserByUsernameAsync(_username);
+                Uri authorizationServerTokenIssuerUri = new Uri(HttpUri.IdentityUri);
 
-                userFromBase = await Get(usernameString);
-               
-                if (userFromBase.Password == passwordString)
-                {
-                    Intent intent = new Intent(this, typeof(General));
-                    intent.PutExtra("user", JsonConvert.SerializeObject(userFromBase));
-                    StartActivity(intent);
-                }
-                else
-                {
-                    correction.Text = "Invalid email or password";
-                }
-                
+                string clientId = "client";
+                string clientSecret = "secret";
+                string scope = "UserService";
+                string username = usernameString;
+                string password = passwordString;
+
+                //Get token for the specified clientId, clientSecret, and scope
+                string token = await RequestTokenToAuthorizationServer(authorizationServerTokenIssuerUri, clientId, scope, clientSecret, username, password);
+
+                //Deserialize jwt token
+                AuthServerResponse authServerToken = Newtonsoft.Json.JsonConvert.DeserializeObject<AuthServerResponse>(token);
+
+                check.Text = authServerToken.access_token;
+
+                //secured web api request
+                string response = SecureWebApiCall(authServerToken).GetAwaiter().GetResult();
+                Console.WriteLine("Response received from WebAPI:");
+                Console.WriteLine(response);
+                Console.ReadKey();
+
+                //------------------------------------------------------------------
+                //userFromBase = await Get(usernameString);
+
+                //if (userFromBase.Password == passwordString)
+                //{
+                //    Intent intent = new Intent(this, typeof(General));
+                //    intent.PutExtra("user", JsonConvert.SerializeObject(userFromBase));
+                //    StartActivity(intent);
+                //}
+                //else
+                //{
+                //    correction.Text = "Invalid email or password";
+                //}
             }
         }
 
         public async Task<User> Get(string username)
         {
-            
+
             HttpClient client = new HttpClient();
             try
             {
@@ -96,6 +113,68 @@ namespace Remuse
             {
                 throw new Exception();
             }
+        }
+
+        /// <summary>
+        /// This method requests Identity Server to get access token.
+        /// </summary>
+        /// <param name="uriAuthorizationServerUri"></param>
+        /// <param name="clientId"></param>
+        /// <param name="scope"></param>
+        /// <param name="clientSecret"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        private static async Task<string> RequestTokenToAuthorizationServer(Uri uriAuthorizationServerUri, string clientId, string scope, string clientSecret, string username, string password)
+        {
+            HttpResponseMessage responseMessage = new HttpResponseMessage();
+
+            using (HttpClient client = new HttpClient())
+            {
+                // returns DiscoveryResponse 
+                client.BaseAddress = uriAuthorizationServerUri;
+                var disco = client.GetDiscoveryDocumentAsync(uriAuthorizationServerUri.ToString()).Result;
+
+                HttpRequestMessage tokenRequest = new HttpRequestMessage(HttpMethod.Post, disco.TokenEndpoint);
+                HttpContent httpContent = new FormUrlEncodedContent(
+                    new[]
+                    {
+                        new KeyValuePair<string, string>("grant_type", "password"),
+                        new KeyValuePair<string, string>("client_id", clientId),
+                        new KeyValuePair<string, string>("scope", scope),
+                        new KeyValuePair<string, string>("client_secret", clientSecret),
+                        new KeyValuePair<string, string>("username", username),
+                        new KeyValuePair<string, string>("password", password)
+                    });
+
+                tokenRequest.Content = httpContent;
+                responseMessage = await client.SendAsync(tokenRequest);
+            }
+            return await responseMessage.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// All methods must be similar to this method.
+        /// </summary>
+        /// <param name="authorizationServerToken"></param>
+        /// <returns></returns>
+        private static async Task<string> SecureWebApiCall(AuthServerResponse authorizationServerToken)
+        {
+
+            string uri = "http://localhost:44383/api/books";
+
+            HttpResponseMessage responseMessage;
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authorizationServerToken.access_token);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
+                responseMessage = await httpClient.SendAsync(request);
+
+                ////Equivalent to above three lines (lines 65-67)
+                //httpClient.SetBearerToken(authorizationServerToken.AccessToken);
+                //var response = await httpClient.GetAsync("uri");
+            }
+            return await responseMessage.Content.ReadAsStringAsync();
         }
     }
 }
