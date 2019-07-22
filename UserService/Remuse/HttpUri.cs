6603ad2cfe1b8091c4c56975ae9790dc93f7c284
Serob Hayrapetyan <serob.airapetyan@mail.ru>
2019-07-22 18:00:49 +0400

    Something new

--- UserService/Remuse/HttpUri.cs
+++ UserService/Remuse/HttpUri.cs
@@ -2,10 +2,10 @@
 {
     public static class HttpUri
     {
-        public static string AuthorizeUri = "http://192.168.1.13:45455/";
-        public static string BookUri = "http://192.168.1.13:45457/";
-        public static string UserUri = "http://192.168.1.13:45461/";
-        public static string ProfileApiUri = "http://192.168.1.13:45459/";
+        public static string AuthorizeUri = "http://192.168.88.238:45455/"; //84
+        public static string BookUri = "http://192.168.88.238:45457/";      //85
+        public static string UserUri = "http://192.168.88.238:45461/";      //82
+        public static string ProfileApiUri = "http://192.168.88.238:45459/";//83
         public static string AdminUri = "";
     }
 }
